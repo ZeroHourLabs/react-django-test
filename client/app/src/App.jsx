6c535daa578b46742d858248150df01b90f262e2
221B0825 <eunseoyu0825@gmail.com>
2025-02-21 15:29:13 +0900

    feat: add addBook function

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -5,55 +5,65 @@ import './App.css'
 
 function App() {
   const[books, setBooks] = useState([]);
+  const[title, setTitle] = useState("")
+  const[releaseYear, setReleaseYear] = useState(0);
 
   useEffect(() => {
     fetchBooks();
   }, []);
 
 
+
   const fetchBooks = async () => {
-    try{
+    try {
       const response = await fetch("http://127.0.0.1:8000/api/books/");
-      const data = await response.json()
-      console.log(data);
-    } catch(error) {
+      const data = await response.json();
+      setBooks(data);
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  const [count, setCount] = useState(0)
+  const addBook = async () => {
+    const bookData = {
+      title,
+      release_date: releaseYear
+    };
+
+    try{
+      const response = await fetch("http://127.0.0.1:8000/api/books/create/", {
+        method: "POST",
+        headers: {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(bookData)
+      });
+  
+      const data = await response.json();
+      setBooks((prev) => [...prev, data]);
+
+    }catch(error){
+      console.log(error);
+    }
+
+  }
 
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-
       <div>
         <h1>Book Website</h1>
 
         <div>
-          <input type='text' placeholder='Boot Title...'></input>
-          <input type='number' placeholder='Release Date...'></input>
-          <button> Add Book </button>
+          <input type='text' placeholder='Boot Title...' onChange={(e) => setTitle(e.target.value)}></input>
+          <input type='number' placeholder='Release Date...' onChange={(e) => setReleaseYear(e.target.value)}></input>
+          <button onClick={addBook}> Add Book </button>
         </div>
+        {books.map((book) => (
+        <div>
+          <p>Title: {book.title}</p>
+          <p>Release Year: {book.release_date} </p>
+          </div>
+          ))}
       </div>
     </>
   )
